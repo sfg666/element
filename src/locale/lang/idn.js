502ab8afdfdf82656229252f757890ec719e9718
shufugeng <ieldentukzovb@gmail.com>
2022-09-13 10:46:48 +0800

    add hindi

---
+++ src/locale/lang/idn.js
@@ -0,0 +1,124 @@
+export default {
+    el: {
+      colorpicker: {
+        confirm: 'ज़रूर',
+        clear: 'खाली'
+      },
+      datepicker: {
+        now: 'अभी व',
+        today: 'आज',
+        cancel: 'रद्द करना',
+        clear: 'खाली',
+        confirm: 'ज़रूर',
+        selectDate: 'तारीख़ चुनें',
+        selectTime: 'चयन अवधि',
+        startDate: 'आरंभ करने की तिथि',
+        startTime: 'प्रारम्भ का समय',
+        endDate: 'अंतिम तिथि',
+        endTime: 'अंत समय',
+        prevYear: 'पिछले वर्ष',
+        nextYear: 'आगामी वर्ष',
+        prevMonth: 'पिछले महीने',
+        nextMonth: 'अगले महीने',
+        year: 'साल',
+        month1: 'जनवरी',
+        month2: 'फ़रवरी',
+        month3: 'मार्च',
+        month4: 'अप्रैल',
+        month5: 'मई',
+        month6: 'जून',
+        month7: 'जुलाई',
+        month8: 'अगस्त',
+        month9: 'सितंबर',
+        month10: 'अक्टूबर',
+        month11: 'नवंबर',
+        month12: 'दिसंबर',
+        // week: '周次',
+        weeks: {
+          sun: 'दिन',
+          mon: 'एक',
+          tue: 'दो',
+          wed: 'तीन',
+          thu: 'चार',
+          fri: 'पांच',
+          sat: 'छह'
+        },
+        months: {
+          jan: 'जनवरी',
+          feb: 'फ़रवरी',
+          mar: 'मार्च',
+          apr: 'अप्रैल',
+          may: 'मई',
+          jun: 'जून',
+          jul: 'जुलाई',
+          aug: 'अगस्त',
+          sep: 'सितंबर',
+          oct: 'अक्टूबर',
+          nov: 'नवंबर',
+          dec: 'दिसंबर'
+        }
+      },
+      select: {
+        loading: 'लोड हो रहा है',
+        noMatch: 'कोई मिलान डेटा नहीं',
+        noData: 'कोई डेटा नहीं',
+        placeholder: 'कृपया चुनें'
+      },
+      cascader: {
+        noMatch: 'कोई मिलान डेटा नहीं',
+        loading: 'लोड हो रहा है',
+        placeholder: 'कृपया चुनें',
+        noData: 'कोई डेटा नहीं'
+      },
+      pagination: {
+        goto: 'के लिए जाओ',
+        pagesize: 'लेख / पृष्ठ',
+        total: 'सामान्य {total} पट्टी',
+        pageClassifier: 'पृष्ठ'
+      },
+      messagebox: {
+        title: 'संकेत देना',
+        confirm: 'ज़रूर',
+        cancel: 'रद्द करना',
+        error: 'दर्ज किया गया डेटा अमान्य है'
+      },
+      upload: {
+        deleteTip: 'के अनुसार मिटाना हटाने की कुंजी',
+        delete: 'मिटाना',
+        preview: 'छवि देखें',
+        continue: 'अपलोड करना जारी रखें'
+      },
+      table: {
+        emptyText: 'कोई डेटा नहीं',
+        confirmFilter: 'फिल्टर',
+        resetFilter: 'रीसेट',
+        clearFilter: 'सब',
+        sumText: 'कुल'
+      },
+      tree: {
+        emptyText: 'कोई डेटा नहीं'
+      },
+      transfer: {
+        noMatch: 'कोई मिलान डेटा नहीं',
+        noData: 'कोई डेटा नहीं',
+        titles: ['सूची 1', 'सूची  2'],
+        filterPlaceholder: 'खोज सामग्री दर्ज करें',
+        noCheckedFormat: 'सामान्य {total} वस्तु',
+        hasCheckedFormat: 'चयनित已选 {checked}/{total} वस्तु'
+      },
+      image: {
+        error: 'लोड करने में विफल'
+      },
+      pageHeader: {
+        title: 'वापसी'
+      },
+      popconfirm: {
+        confirmButtonText: 'ज़रूर',
+        cancelButtonText: 'रद्द करना'
+      },
+      empty: {
+        description: 'कोई डेटा नहीं'
+      }
+    }
+  };
+  
